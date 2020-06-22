493795dfba44522e0499706fb9e9e8948976183e
leonardomso <leonardomso11@gmail.com>
2020-06-22 12:32:47 -0300

    First commit

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+const Index = () => {
+  return (
+    <div className="container">
+      <Head>
+        <title>Leonardo Maldonado - Software Engineer</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div>
+        <h1>Website</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Index;
