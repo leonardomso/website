6d6805205ec849c3209c5ca17a218229902e0725
leonardomso <leonardomso11@gmail.com>
2021-02-17 16:52:18 -0300

    Starting updates

--- next.config.js
+++ next.config.js
@@ -1,19 +1,12 @@
-const withPlugins = require("next-compose-plugins");
-const withPWA = require("next-pwa");
+// const withPlugins = require("next-compose-plugins");
+// const withPWA = require("next-pwa");
 
-module.exports = withPlugins([
-  [
-    withPWA({
-      pwa: {
-        dest: "public",
-      },
-    }),
-  ],
-  [
-    {
-      env: {
-        FATHOM_ID: process.env.FATHOM_ID,
-      },
-    },
-  ],
-]);
+// module.exports = withPlugins([
+//   [
+//     withPWA({
+//       pwa: {
+//         dest: "public",
+//       },
+//     }),
+//   ],
+// ]);
