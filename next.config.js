7df11cc8f537711aebb2e06063cb99a6726a26a8
Leonardo Maldonado <leonardomso11@gmail.com>
2023-07-26 08:44:45 -0300

    Installed dependencies and added some changes on next config.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,42 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const headers = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+]
+
+if (process.env.FORCE_SSL) {
+  headers.push({
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  })
+}
+
+module.exports = {
+  reactStrictMode: true,
+  swcMinify: true,
   experimental: {
     appDir: true,
   },
+  images: {
+    domains: [
+      'lh3.googleusercontent.com',
+      'avatars.githubusercontent.com',
+      'images.unsplash.com',
+      'cdn.sanity.io',
+      'pbs.twimg.com',
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers,
+      },
+    ]
+  },
 }
-
-module.exports = nextConfig
