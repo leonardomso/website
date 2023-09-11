38d9ebdb5e9364aa31b7dcf52fb41b9cc5cb307d
Leonardo Maldonado <leonardomso11@gmail.com>
2023-09-11 14:03:20 -0300

    Removed unused dependencies and added simple analytics script.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
+import Script from 'next/script'
 
 import Footer from '~/app/components/Footer/Footer'
 
@@ -58,6 +59,7 @@ const Layout = ({ children }: Props) => {
         </header>
         {children}
         <Footer />
+        <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
       </body>
     </html>
   )
